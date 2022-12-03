b70b756fb70ac998b804632697020dd97af0da59
Kayke Campos <49199038+krenancampos@users.noreply.github.com>
2022-12-03 20:35:38 +0000

    Commit 14

--- Movie.java
+++ Movie.java
@@ -39,10 +39,9 @@ public class Movie {
   public double getCharge(int daysRented) {
     return _price.getCharge(daysRented);
   }
-  
-  public int getFrequentRenterPoints(int daysRented){
-    if ((getPriceCode() == Movie.NEW_RELEASE) &&
-         daysRented > 1) return 2;
-    return 1;
+
+  public int getFrequentRenterPoints(int daysRented) {
+      return _price.getFrequentRenterPoints(daysRented);
   }
+
 }
